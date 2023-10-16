bbea9a1d67fcf460e7aee4f3d554f916fbcfd285
KyrylBudanov <boris201393@gmail.com>
2023-10-16 14:42:35 +0300

    Перший коміт

---
+++ laba4(1)/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Базовий клас "Живий організм"
+class Organism
+{
+    public double Energy { get; set; }
+    public int Age { get; set; }
+    public double Size { get; set; }
+
+    public Organism(double energy, int age, double size)
+    {
+        Energy = energy;
+        Age = age;
+        Size = size;
+    }
+}
+
+// Спадкоємні класи
+class Animal : Organism, IReproducible, IPredator
+{
+    public string Species { get; set; }
+    public int ReproductionRate { get; set; }
+    public double PreyPreference { get; set; }
+
+    public Animal(double energy, int age, double size, string species, int reproductionRate, double preyPreference)
+        : base(energy, age, size)
+    {
+        Species = species;
+        ReproductionRate = reproductionRate;
+        PreyPreference = preyPreference;
+    }
+
+    public void Reproduce()
+    {
+        // Логіка розмноження для тварин
+    }
+
+    public void Hunt()
+    {
+        // Логіка полювання для тварин
+    }
+}
+
+class Plant : Organism, IReproducible
+{
+    public string Type { get; set; }
+
+    public Plant(double energy, int age, double size, string type)
+        : base(energy, age, size)
+    {
+        Type = type;
+    }
+
+    public void Reproduce()
+    {
+        // Логіка розмноження для рослин
+    }
+}
+
+class Microorganism : Organism, IReproducible
+{
+    public string MicroType { get; set; }
+
+    public Microorganism(double energy, int age, double size, string microType)
+        : base(energy, age, size)
+    {
+        MicroType = microType;
+    }
+
+    public void Reproduce()
+    {
+        // Логіка розмноження для мікроорганізмів
+    }
+}
+
+// Інтерфейси
+interface IReproducible
+{
+    void Reproduce();
+}
+
+interface IPredator
+{
+    void Hunt();
+}
+
+// Клас "Екосистема"
+class Ecosystem
+{
+    private List<Organism> organisms = new List<Organism>();
+
+    public void AddOrganism(Organism organism)
+    {
+        organisms.Add(organism);
+    }
+
+    public void SimulateEcology()
+    {
+        // Логіка взаємодії організмів в екосистемі: харчування, розмноження, конкуренція
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Ecosystem ecosystem = new Ecosystem();
+
+        // Створіть об'єкти тварин, рослин, мікроорганізмів і додайте їх до екосистеми
+        Animal lion = new Animal(100, 5, 1.5, "Lion", 3, 0.7);
+        Animal zebra = new Animal(80, 4, 1.2, "Zebra", 4, 0.5);
+        Plant grass = new Plant(50, 2, 0.1, "Grass");
+        Microorganism bacteria = new Microorganism(10, 1, 0.01, "Bacteria");
+
+        ecosystem.AddOrganism(lion);
+        ecosystem.AddOrganism(zebra);
+        ecosystem.AddOrganism(grass);
+        ecosystem.AddOrganism(bacteria);
+
+        // Симулюйте взаємодію організмів у екосистемі
+        ecosystem.SimulateEcology();
+    }
+}
---
+++ laba4(2)/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Базовий клас "Комп'ютер"
+class Computer
+{
+    public string IPAddress { get; set; }
+    public int Power { get; set; }
+    public string OperatingSystem { get; set; }
+
+    public Computer(string ipAddress, int power, string os)
+    {
+        IPAddress = ipAddress;
+        Power = power;
+        OperatingSystem = os;
+    }
+}
+
+// Спадкоємні класи
+class Server : Computer, IConnectable
+{
+    public List<Computer> ConnectedComputers { get; private set; }
+
+    public Server(string ipAddress, int power, string os)
+        : base(ipAddress, power, os)
+    {
+        ConnectedComputers = new List<Computer>();
+    }
+
+    public void Connect(Computer computer)
+    {
+        ConnectedComputers.Add(computer);
+        Console.WriteLine($"Сервер {IPAddress} підключився до комп'ютера {computer.IPAddress}");
+    }
+
+    public void Disconnect(Computer computer)
+    {
+        ConnectedComputers.Remove(computer);
+        Console.WriteLine($"Сервер {IPAddress} відключився від комп'ютера {computer.IPAddress}");
+    }
+
+    public void TransferData(Computer computer, string data)
+    {
+        Console.WriteLine($"Сервер {IPAddress} передав дані до комп'ютера {computer.IPAddress}: {data}");
+    }
+}
+
+class Workstation : Computer, IConnectable
+{
+    public List<Computer> ConnectedComputers { get; private set; }
+
+    public Workstation(string ipAddress, int power, string os)
+        : base(ipAddress, power, os)
+    {
+        ConnectedComputers = new List<Computer>();
+    }
+
+    public void Connect(Computer computer)
+    {
+        ConnectedComputers.Add(computer);
+        Console.WriteLine($"Робоча станція {IPAddress} підключилась до комп'ютера {computer.IPAddress}");
+    }
+
+    public void Disconnect(Computer computer)
+    {
+        ConnectedComputers.Remove(computer);
+        Console.WriteLine($"Робоча станція {IPAddress} відключилась від комп'ютера {computer.IPAddress}");
+    }
+
+    public void TransferData(Computer computer, string data)
+    {
+        Console.WriteLine($"Робоча станція {IPAddress} передала дані до комп'ютера {computer.IPAddress}: {data}");
+    }
+}
+
+class Router : Computer, IConnectable
+{
+    public List<Computer> ConnectedComputers { get; private set; }
+
+    public Router(string ipAddress, int power, string os)
+        : base(ipAddress, power, os)
+    {
+        ConnectedComputers = new List<Computer>();
+    }
+
+    public void Connect(Computer computer)
+    {
+        ConnectedComputers.Add(computer);
+        Console.WriteLine($"Маршрутизатор {IPAddress} підключився до комп'ютера {computer.IPAddress}");
+    }
+
+    public void Disconnect(Computer computer)
+    {
+        ConnectedComputers.Remove(computer);
+        Console.WriteLine($"Маршрутизатор {IPAddress} відключився від комп'ютера {computer.IPAddress}");
+    }
+
+    public void TransferData(Computer computer, string data)
+    {
+        Console.WriteLine($"Маршрутизатор {IPAddress} передав дані до комп'ютера {computer.IPAddress}: {data}");
+    }
+}
+
+// Інтерфейс "IConnectable"
+interface IConnectable
+{
+    void Connect(Computer computer);
+    void Disconnect(Computer computer);
+    void TransferData(Computer computer, string data);
+}
+
+// Клас "Мережа"
+class Network
+{
+    public List<Computer> AllComputers { get; private set; }
+
+    public Network()
+    {
+        AllComputers = new List<Computer>();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Network network = new Network();
+
+        Server server1 = new Server("192.168.1.1", 1000, "Windows Server");
+        Server server2 = new Server("192.168.1.2", 1200, "Linux");
+        Workstation workstation1 = new Workstation("192.168.1.3", 800, "Windows 10");
+        Workstation workstation2 = new Workstation("192.168.1.4", 900, "macOS");
+        Router router = new Router("192.168.1.5", 500, "RouterOS");
+
+        network.AllComputers.Add(server1);
+        network.AllComputers.Add(server2);
+        network.AllComputers.Add(workstation1);
+        network.AllComputers.Add(workstation2);
+        network.AllComputers.Add(router);
+
+        server1.Connect(workstation1);
+        server2.Connect(workstation2);
+        router.Connect(server1);
+        router.Connect(server2);
+
+        workstation1.TransferData(server1, "Дані від робочої станції до сервера 1");
+        server1.TransferData(workstation1, "Відповідь від сервера 1 до робочої станції");
+
+        router.Disconnect(server1);
+        server2.Disconnect(workstation2);
+
+        Console.ReadKey();
+    }
+}
